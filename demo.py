694323f239c5be12ab57dd3bef50d5d5d2039270
Water <lxhui2shui@gmail.com>
2019-03-25 22:09:01 +0800

    test

---
+++ demo.py
@@ -0,0 +1,13 @@
+'''
+grade = [88, 98, 67, 54, 65, 54, 98, 34, 56, 78, 46, 77, 33, 44, 55, 66]
+a = []
+b = []
+for i in grade:
+    if i > 80:
+        a.append(i)
+    else:
+        b.append(i)
+
+print(a)
+print(b)
+'''
